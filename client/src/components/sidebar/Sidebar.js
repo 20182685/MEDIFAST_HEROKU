8972afca8996ec5991d21ecf934482fec903be37
Sebastian Guevara <20182685@aloe.ulima.edu.pe>
2022-07-19 14:28:35 -0500

    Se configuró el sidebar

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -7,13 +7,13 @@ const Sidebar = () => {
       <nav id='sidebarMenu' className='collapse d-lg-block sidebar collapse bg-white'>
         <div className='position-sticky'>
           <div className='list-group list-group-flush mx-3 mt-4'>
-            <a className='list-group-item list-group-item-action py-2 ripple active'>
+            <a href='https://medifast-app-heroku.herokuapp.com/appointments' className='list-group-item list-group-item-action py-2 ripple active'>
               <i className='fas fa-calendar-alt fa-fw me-3'></i><span>Citas</span></a>
-            <a className='list-group-item list-group-item-action py-2 ripple'>
+            <a href='https://medifast-app-heroku.herokuapp.com/consulting' className='list-group-item list-group-item-action py-2 ripple'>
               <i className='fas fa-clipboard-user fa-fw me-3'></i><span>Pacientes</span></a>
-            <a className='list-group-item list-group-item-action py-2 ripple'>
+            <a href='https://medifast-app-heroku.herokuapp.com/messaging' className='list-group-item list-group-item-action py-2 ripple'>
               <i className='fas fa-comment-medical fa-fw me-3'></i><span>Mensajes</span></a>
-            <a className='list-group-item list-group-item-action py-2 ripple'>
+            <a href='https://medifast-app-heroku.herokuapp.com/reviews' className='list-group-item list-group-item-action py-2 ripple'>
               <i className='fas fa-heart fa-fw me-3'></i><span>Comentarios</span></a>
             <a className='list-group-item list-group-item-action py-2 ripple'>
               <i className='fas fa-lock fa-fw me-3'></i><span>Salir</span></a>
